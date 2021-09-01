b66028c7de7d66abca2a9c043783fc99285ce3d4
Saddam-tech <salokhiddinov0727@gmail.com>
2021-09-02 05:34:10 +0900

    latest updates

---
+++ src/components/comments/Comments.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import CommentsList from "./CommentsList";
+
+const Comments = () => {
+  const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
+
+ // Start from here //////////////////
+ ////////////////////////////////////
+ 
+};
+
+export default Comments;
