286d3f18578faf49b23d3b57dfdab2c51148f1de
Saddam-tech <salokhiddinov0727@gmail.com>
2021-09-03 06:06:07 +0900

    comments section added

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -20,9 +20,45 @@ const Comments = () => {
     sendRequest(quoteId);
   }, [quoteId, sendRequest]);
 
- // Start from here //////////////////
- ////////////////////////////////////
- 
+  const startAddCommentHandler = () => {
+    setIsAddingComment(true);
+  };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "completed" && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  return (
+    <section className={classes.comments}>
+      <h2>User Comments</h2>
+      {!isAddingComment && (
+        <button className="btn" onClick={startAddCommentHandler}>
+          Add a Comment
+        </button>
+      )}
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      {comments}
+    </section>
+  );
 };
 
 export default Comments;
